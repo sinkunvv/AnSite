934ccd12122c9df821d13e4a0cc960e3b66bc5b2
sinkunvv <sinkunvv@gmail.com>
2017-09-19 23:35:30 +0900

    day18 commit

--- antenna/app/Console/Kernel.php
+++ antenna/app/Console/Kernel.php
@@ -24,8 +24,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule
+        ->call('RssController@update')
+        ->withoutOverlapping()
+        ->everyMinute();
     }
 
     /**
